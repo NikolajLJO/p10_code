4a614c911b5c8fb59edf7dd3210943fb7dc13b1f
Lars Svane Jensen <lsje16@student.aau.dk>
2021-03-30 09:25:14 +0200

    memory size and replaze strategy

--- lars_pellet/memory.py
+++ lars_pellet/memory.py
@@ -5,12 +5,18 @@ functions to query it.
 
 
 class ReplayMemory():
-    def __init__(self, batch_size = 32):
+    def __init__(self, batch_size = 32, max_memory_size = 10000):
         self.memory = []
         self.batch_size = batch_size
+        self.memory_refrence_pointer = 0
+        self.MAX_MEMORY_SIZE = max_memory_size
     
-    def save(self, state, action, reward, terminating, state_prime):
-        self.memory.append([state, action, reward, terminating, state_prime])
+    def save(self, state, action, reward, terminating, state_prime): 
+        if len(memory) < memory_size:
+            self.memory.append([state, action, reward, terminating, state_prime])
+        else:
+            memory[memory_replace_pointer] = transition
+            self.memory_replace_pointer = (self.memory_replace_pointer + 1) % memory_size
 
     def sample(self):
         return = self.memory.sample(self.replay_batch_size)
@@ -18,3 +24,9 @@ class ReplayMemory():
     def sampleEEminibatch():
         raise NotImplementedError
 
+
+    else:
+        memory[memory_replace_pointer] = transition
+        memory_replace_pointer = (memory_replace_pointer + 1) % memory_size
+    
+    return memory, memory_replace_pointer
