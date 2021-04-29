29ffe4afd0254d256b3ae7ed335fcaf929052dbd
Lars Svane Jensen <lsje16@student.aau.dk>
2021-04-29 10:22:11 +0200

    linted train

--- python_pellet/train.py
+++ python_pellet/train.py
@@ -1,42 +1,60 @@
-import numpy as np
-import torch
-from random import sample
-from init import setup
-import sys
-import multiprocessing as mp
+'''
+Train is the main file when you wich to train an agent
+it uses some commandline parameters these are:
+    args1 = gamename
+    args2 = traenigsperiode
+    args3 = network update frequency
+    args4 = partition update frequency
+'''
+
 import os
-from pathlib import Path
 import datetime
 import logging
 import time
+import sys
+from pathlib import Path
+import numpy as np
+import torch
 import torchvision.transforms as transforms
+from init import setup
 
 transform_to_image = transforms.ToPILImage()
 
+# Partition constants
 MAX_PARTITIONS = 100
-start_making_partitions = 2000000
-partition_add_time_mult = 1.2
-start_eelearn = 250000
-end_eelearn = 2000000
+START_MAKING_PARTITIONS = 2000000
+PARTITION_ADD_TIME_MULT = 1.2
 
-start_qlearn = 2250000
-update_targets_frequency = 10000
-save_networks_frequency = 500000
+# EE and RND constants
+START_EELEARN = 250000
+END_EELEARN = 2000000
 
+# Q constants
+START_QLEARN = 2250000
 
-'''
-args1 = gamename
-args2 = traenigsperiode
-args3 = network update frequency
-args4 = partition update frequency
-''' 
+# Shared constants
+UPDATE_TARGETS_FREQUENCY = 10000
+SAVE_NETWORKS_FREQUENCY = 500000
 
 def get_writer():
+    '''
+    this function return the OS writer
+    Return: OS writer
+    '''
     _, writer = os.pipe()
     return os.fdopen(writer, 'w')
 
 
 def mainloop(args):
+    '''
+    mainloop runs the main training and calls all other functions and methods as necessary
+    Input: args = [stdinput,
+                   gamename,
+                   trainingsteps,
+                   network_update_frequency,
+                   partition_update_frequency,
+                   use_RND]
+    '''
     torch.set_flush_denormal(True)
     path = Path(__file__).parent
     Path(path / 'logs').mkdir(parents=True, exist_ok=True)
@@ -48,7 +66,7 @@ def mainloop(args):
                         filemode='w')
     logger = get_writer()
     sys.stdout = logger
-    
+
     partition_candidate = None
     terminating = False
     total_score = 0
@@ -57,10 +75,10 @@ def mainloop(args):
     distance = 0
     episode_buffer = []
 
-    game_actions, replay_memory, agent, opt, env = setup(args[1], args[5])
+    _, replay_memory, agent, _, env = setup(args[1], args[5])
     add_partition_freq = int(args[4])
     update_freq = int(args[3])
-    
+
     state = env.reset()
     partition_memory = [[state, 0]]
     transform_to_image(state[0][0]).save(str(path) + "/logs/" + "patition_1.png")
@@ -74,18 +92,21 @@ def mainloop(args):
 
         action, policy = agent.find_action(state, i)
 
-        auxiliary_reward = torch.tensor(calculate_auxiliary_reward(policy, action.item()), device=agent.device)
+        auxiliary_reward = torch.tensor(calculate_auxiliary_reward(policy,
+                                                                   action.item()),
+                                        device=agent.device)
 
         if not terminating:
-            state_prime, reward, terminating, info = env.step(action.item())
+            state_prime, reward, terminating, _ = env.step(action.item())
             total_score += reward
             reward = max(min(reward,1),-1)
             if i % 10 == 0:
-                visited, visited_prime, distance = agent.find_current_partition(state_prime, partition_memory)
-            episode_buffer.append([state, action, visited, auxiliary_reward, 
-                                   torch.tensor(reward, device=agent.device).unsqueeze(0), 
-                                   torch.tensor(terminating, device=agent.device).unsqueeze(0), 
-                                   state_prime, 
+                visited, visited_prime, distance = agent.find_current_partition(state_prime,
+                                                                                partition_memory)
+            episode_buffer.append([state, action, visited, auxiliary_reward,
+                                   torch.tensor(reward, device=agent.device).unsqueeze(0),
+                                   torch.tensor(terminating, device=agent.device).unsqueeze(0),
+                                   state_prime,
                                    visited_prime])
         else:
             state_prime = env.reset()
@@ -96,7 +117,11 @@ def mainloop(args):
             visited_prime = []
             replay_memory.save(episode_buffer)
             episode_time = time.process_time()-now
-            logging.info("step: " + str(i) + " total_score: " + str(total_score) + " time taken: " + str(episode_time) + " partitions: " + str(len(partition_memory)) + " time pr. step: " + str(episode_time/len(episode_buffer)))
+            logging.info("step: " + str(i) +
+                         " total_score: " + str(total_score) +
+                         " time taken: " + str(episode_time) +
+                         " partitions: " + str(len(partition_memory)) +
+                         " time pr. step: " + str(episode_time/len(episode_buffer)))
             episode_buffer.clear()
             now = time.process_time()
             total_score = 0
@@ -107,42 +132,49 @@ def mainloop(args):
         else:
             steps_since_reward += 1
 
-        if distance > dmax and i >= start_making_partitions:
+        if distance > dmax and i >= START_MAKING_PARTITIONS:
             partition_candidate = state_prime
             dmax = distance
-        
+
         if i % add_partition_freq == 0 and partition_candidate is not None:
             partition_memory.append([partition_candidate, 0])
             dmax = 0
-            
+
             partition_memory = partition_memory[-MAX_PARTITIONS:]
-            add_partition_freq = int(add_partition_freq * partition_add_time_mult)
-            transform_to_image(state[0][0]).save(logpath + "patition_" + str(len(partition_memory)) + ".png")
+            add_partition_freq = int(add_partition_freq * PARTITION_ADD_TIME_MULT)
+            transform_to_image(state[0][0]).save(logpath + "patition_" +
+                                                 str(len(partition_memory)) + ".png")
 
         state = state_prime
         visited = visited_prime
 
-        if i % update_freq == 0 and i >= start_qlearn:
+        if i % update_freq == 0 and i >= START_QLEARN:
             agent.qlearn(replay_memory)
-        
-        if i % update_freq == 0 and i >= start_eelearn and (i < end_eelearn or args[5]):
+
+        if i % update_freq == 0 and i >= START_QLEARN and (i < END_EELEARN or int(args[5])):
             agent.imagecomparelearn(replay_memory)
-        
-        if i % update_targets_frequency == 0:
+
+        if i % UPDATE_TARGETS_FREQUENCY == 0:
             agent.update_targets()
-            
-        if i % save_networks_frequency == 0:
+
+        if i % SAVE_NETWORKS_FREQUENCY == 0:
             agent.save_networks(logpath, i)
-        
+
         if steps_since_reward > 500:
             terminating = True
             episode_buffer[-1][5] = torch.tensor(terminating, device=agent.device).unsqueeze(0)
             steps_since_reward = 0
-    
+
     agent.save_networks(logpath, i)
 
 
 def calculate_auxiliary_reward(policy, aidx):
+    '''
+    this function calcules the the auxillary reward for each action
+    Input: policy is the calculated values for each action
+           aidx is the index of the used action
+    output: a list of auxillary rewards for the actions in the action space
+    '''
     aux = [0]*(policy.size()[1])
     policy = policy.squeeze(0)
     for i in range(len(aux)):
@@ -152,18 +184,13 @@ def calculate_auxiliary_reward(policy, aidx):
             aux[i] = -policy[i].item()
     return aux
 
-
-def partitiondeterminarion(ee_network, s_n, r):
-    mindist = [np.inf, None]
-    for s_pi in r:
-        dist = ee_network.distance(ee_network, s_n, s_pi[0], r[0][0])
-        if mindist[0] > dist:
-            mindist[0] = dist
-            mindist[1] = s_pi
-    return mindist[1]
-
-
 def update_partitions(visited_partitions, partition_memory):
+    '''
+    this function update the number of visits that
+    each partition has when an episode is terminatied
+    Input: visited_partitions is the list of visited partitions in an episode
+           partition_memory is the full partition memory
+    '''
     for visited in visited_partitions:
         for i, partition in enumerate(partition_memory):
             if torch.equal(visited[0],partition[0]):
@@ -173,9 +200,5 @@ def update_partitions(visited_partitions, partition_memory):
 
 if __name__ == "__main__":
     print("start")
-    '''mp.set_start_method('spawn')
-    with mp.Pool(processes=4) as pool:
-        que = mp.Queue()
-        process = mp.Process(target=mainloop, args=(sys.argv,))
-'''
     mainloop(sys.argv)
+    print("done")
