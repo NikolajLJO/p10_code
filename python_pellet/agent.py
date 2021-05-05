fc02eba4686cc4fb9cea7dd70271eb99642c87b6
Lars Svane Jensen <lsje16@student.aau.dk>
2021-05-05 10:06:20 +0200

    agent doc update

--- python_pellet/agent.py
+++ python_pellet/agent.py
@@ -102,7 +102,7 @@ class Agent:
 
     def eelearn(self, replay_memory):
         '''
-        eelearn calucaltes the variabels used for backpropagating the q-network
+        eelearn calucaltes the variabels used for backpropagating the ee-network
         Input: replay_memory to sample from
         '''
         if len(replay_memory.memory) > replay_memory.batch_size:
@@ -110,7 +110,9 @@ class Agent:
             batch = replay_memory.sample_ee_minibatch()
 
             states, s_primes, smid, auxreward = zip(*batch)
+            # targ_onesteps is a list of targets where the fist step is what is done and future is calculated by a netwok
             targ_onesteps = []
+            # merged is a list of all the merged states used to calculate future
             merged = []
             for i in range(len(smid)):
                 targ_onesteps.append(auxreward[i][0])
@@ -128,7 +130,7 @@ class Agent:
                 targ_mc[i] = torch.sum(discounted_aux, 0)
 
             targ_mix = (1 - self.ne) * targ_onesteps + self.ne * targ_mc
-
+            # merged is changed to contain a list of state calculaing now and future
             merged = []
             for i in range(len(states)):
                 merged.append(merge_states_for_comparason(states[i], s_primes[i]))
