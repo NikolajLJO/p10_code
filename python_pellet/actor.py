1c5a8aa29410d615cd502445f86c70bd7c8d4688
nilj <nilj@netcompany.com>
2021-05-11 11:34:07 +0200

    cach clear

--- python_pellet/actor.py
+++ python_pellet/actor.py
@@ -81,6 +81,8 @@ class Actor:
 					total_score = 0
 					steps_since_reward = 0
 
+					torch.cuda.empty_cache()
+
 				if reward != 0 or len(visited) != len(visited_prime):
 					steps_since_reward = 0
 				else:
