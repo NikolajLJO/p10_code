a9c855a9ab81c69ba9bbbb767ec2a6b6fc38cc6e
nilj <nilj@netcompany.com>
2021-05-13 09:58:53 +0200

    nowait

--- python_pellet/memory.py
+++ python_pellet/memory.py
@@ -4,7 +4,7 @@ import copy
 
 
 class ReplayMemory:
-	def __init__(self, batch_size=32, max_memory_size=900000):
+	def __init__(self, batch_size=32, max_memory_size=750000):
 		self.memory = []
 		self.batch_size = batch_size
 		self.memory_refrence_pointer = 0
