ca9d6cdd809a056b715aeff8aa513120655729eb
Aryan <amoha16@student.aau.dk>
2021-05-24 11:39:55 +0200

    Added recursive and python3.

--- python_pellet/run.sh
+++ python_pellet/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ "$1" == "1" ]; then
     echo "Install requirements.";
-    pip install requirements.txt
-    python train.py BattleZoneDeterministic-v0 10000000 4 20000 0
+    pip3 install -r requirements.txt
+    python3 train.py BattleZoneDeterministic-v0 10000000 4 20000 0
 elif [ "$1" == "0" ]; then
     echo "Do not install requirements.";
-    python train.py BattleZoneDeterministic-v0 10000000 4 20000 0
+    python3 train.py BattleZoneDeterministic-v0 10000000 4 20000 0
 else
     echo "Enter either 0 to not install requirements or 1 to install them."
 fi
