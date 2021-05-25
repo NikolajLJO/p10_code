b6c2d64d0cb2b6459ee8bc369cdfaaeca05ab716
Lars Svane Jensen <lsje16@student.aau.dk>
2021-05-25 13:30:32 +0200

    do not divide time

--- python_pellet/agent.py
+++ python_pellet/agent.py
@@ -58,14 +58,14 @@ class Agent:
 		if len(replay_memory.memory) > 0:
 			self.qlearn(replay_memory, batch_size=min(32, len(replay_memory.memory)))
 		post_learn = time.process_time()
-		logging.info("q learned in: " + "%.2f" % ((post_learn - pre_learn) / 1e9))
+		logging.info("q learned in: " + "%.2f" % (post_learn - pre_learn))
 		if not ee_done:
 			pre_learn = time.process_time()
 			logging.info("eel")
 			if len(ee_memory) > 0:
 				self.eelearn(ee_memory, 32)
 			post_learn = time.process_time()
-			logging.info("ee learned in: " + "%.2f" % ((post_learn - pre_learn) / 1e9))
+			logging.info("ee learned in: " + "%.2f" % (post_learn - pre_learn))
 
 	def qlearn(self, replay_memory, batch_size=None):
 		# Sample random minibatch of transitions
