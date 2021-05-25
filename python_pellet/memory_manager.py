cb2c5ecaeb171cc0825cc863c63ea86521e3cd21
Lars Svane Jensen <lsje16@student.aau.dk>
2021-05-25 16:55:01 +0200

    mem log before que fill

--- python_pellet/memory_manager.py
+++ python_pellet/memory_manager.py
@@ -49,11 +49,13 @@ class MemoryManager:
 			else:
 				if learner_replay_que.qsize() == 0 and replay_mem_len > learner_que_max_size:
 					pre = learner_replay_que.qsize()
+					logging.info("refill learner replay mem")
 					self.fill_learner_replay_que(learner_replay_que, learner_que_max_size)
 					logging.info("refilled learner replay mem with |" + str(learner_replay_que.qsize() - pre) + "| elements")
 
 				if learner_ee_que.qsize() == 0 and replay_mem_len > learner_ee_que_max_size:
 					pre = learner_ee_que.qsize()
+					logging.info("refill learner ee mem")
 					self.fill_learner_ee_que(learner_ee_que, learner_ee_que_max_size)
 					logging.info("refilled learner ee mem with |" + str(learner_ee_que.qsize() - pre) + "| elements")
 
